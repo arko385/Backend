4e6bca6451af1387375021c4ec394b8363b6cd76
arko385 <90547765+arko385@users.noreply.github.com>
2024-02-19 23:21:00 +0530

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express=require("express") ;
 const app=express();
 //process.env.PORT not working
-const port=4000||process.env.PORT;
+const port=process.env.PORT||3000;
 
 const template='<div style="color:blue; display: flex; justify-content: center;"><img src="https://media.istockphoto.com/id/1455658894/photo/system-hacked-warning-alert-on-notebook-cyber-attack-on-computer-network-virus-spyware.jpg?s=2048x2048&w=is&k=20&c=Np9GhBSzgUM1d3NFxOLY1E94-XkeitDFk3LCOKpg90g=" width="600" height="400"></div>';
 
