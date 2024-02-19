6f5df74ad85d7c420f79db1903b3f14d540f68ce
arko385 <90547765+arko385@users.noreply.github.com>
2024-02-19 23:13:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express=require("express") ;
+const app=express();
+//process.env.PORT not working
+const port=4000||process.env.PORT;
+
+const template='<div style="color:blue; display: flex; justify-content: center;"><img src="https://media.istockphoto.com/id/1455658894/photo/system-hacked-warning-alert-on-notebook-cyber-attack-on-computer-network-virus-spyware.jpg?s=2048x2048&w=is&k=20&c=Np9GhBSzgUM1d3NFxOLY1E94-XkeitDFk3LCOKpg90g=" width="600" height="400"></div>';
+
+app.get("/",(req,res)=>{
+    res.send("Great learning");
+    console.log("Hi I am good.");
+})
+app.get('/login',(req,res)=>{
+    res.send(template);
+})  
+
+app.listen(port,()=>{
+    console.log(`The server is running on port ${port}.`);
+})
